eb9ae3f97f74121ad47b24c9dd52e9c419e8d858
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-07-18 03:28:17 +0300

    Add quotes to prevent word splitting

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,16 +12,16 @@ else
   pull_request_id="$INPUT_PULL_REQUEST_ID"
 fi
 
-repository_name=$(basename $GITHUB_REPOSITORY)
+repository_name="$(basename "$GITHUB_REPOSITORY")"
 recreated_runner_dir="/home/runner/work/$repository_name"
-mkdir -p $recreated_runner_dir
+mkdir -p "$recreated_runner_dir"
 recreated_repo_dir="$recreated_runner_dir/$repository_name"
 
-ln -s $(pwd) $recreated_repo_dir
+ln -s "$(pwd)" "$recreated_repo_dir"
 
-cd $recreated_repo_dir
+cd "$recreated_repo_dir"
 
 eval python3 /action/run_action.py \
-  --clang-tidy-fixes $INPUT_CLANG_TIDY_FIXES \
-  --pull-request-id $pull_request_id \
-  --repository-root $recreated_repo_dir
+  --clang-tidy-fixes "$INPUT_CLANG_TIDY_FIXES" \
+  --pull-request-id "$pull_request_id" \
+  --repository-root "$recreated_repo_dir"
