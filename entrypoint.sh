deb686d19e102d1a610acd67b3a9f3327bd975fc
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-07-18 03:41:10 +0300

    Add the pull_request_id parameter to support workflow_run events

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -xeu
 
-pull_request_id=$(cat "$GITHUB_EVENT_PATH" | jq 'if (.issue.number != null) then .issue.number else .number end')
+if [ -z "$INPUT_PULL_REQUEST_ID" ]; then
+  pull_request_id=$(cat "$GITHUB_EVENT_PATH" | jq 'if (.issue.number != null) then .issue.number else .number end')
 
-if [ $pull_request_id == "null" ]; then
-  echo "Could not find a pull request ID. Is this a pull request?"
-  exit 0
+  if [ "$pull_request_id" == "null" ]; then
+    echo "Could not find a pull request ID. Is this a pull request?"
+    exit 0
+  fi
+else
+  pull_request_id="$INPUT_PULL_REQUEST_ID"
 fi
 
 repository_name=$(basename $GITHUB_REPOSITORY)
